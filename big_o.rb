0a06fa91d312d3a3ad2bf7b815f2e8187736cf03
Alec Johnson <alecjohnson55@gmail.com>
2017-07-03 12:32:01 -0700

    add arr_1

--- big_o.rb
+++ big_o.rb
@@ -8,3 +8,15 @@ end
 
 # the time complexity is O(1), with a constant factor of 4. if, >, +/-, return
 # the space complexity is O(1), the function will only ever use 2 numbers as memory
+
+
+def print_arr_1(arr)
+  arr.each do |idx|
+    puts idx
+  end
+end
+
+# constant space, but linear time complexity. we don't assign any additional memory,
+# but as the array grows we have to do a linear amount more of prints. constant
+# factor of 1
+
