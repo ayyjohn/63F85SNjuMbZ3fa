635d887a39731cd90c6bf64fac4125d0657dfec4
Alec Johnson <alecjohnson55@gmail.com>
2017-07-03 12:34:55 -0700

    add print_arr_2

--- big_o.rb
+++ big_o.rb
@@ -20,3 +20,15 @@ end
 # but as the array grows we have to do a linear amount more of prints. constant
 # factor of 1
 
+def print_arr_2(arr)
+  arr.each_with_index do |el, idx|
+    break if idx == arr.length/2 - 1
+    puts el
+  end
+end
+
+# the constant factor here will change but the overall time complexity and space complexity
+# are the same, because a constant factor change on time doesn't change the overall value,
+# despite the fact that this function will only ever do half the printing.
+# constant factor is 5/2, if, equality, length, division, and subtraction, but also the function
+# will always stop halfway through the array.
