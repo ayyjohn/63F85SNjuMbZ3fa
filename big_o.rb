77b6d304c733955d480b4e5dedcb285d886e3ce5
Alec Johnson <alecjohnson55@gmail.com>
2017-07-03 12:30:08 -0700

    add space complexity

--- big_o.rb
+++ big_o.rb
@@ -7,4 +7,4 @@ def add(a, b)
 end
 
 # the time complexity is O(1), with a constant factor of 4. if, >, +/-, return
-
+# the space complexity is O(1), the function will only ever use 2 numbers as memory
