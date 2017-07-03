b9dd2c09e2d35243e1fdb6b01767d0037a50731d
Alec Johnson <alecjohnson55@gmail.com>
2017-07-03 14:43:19 -0700

    add random pivot version

--- quicksort.rb
+++ quicksort.rb
@@ -4,6 +4,7 @@
 
 class Array
   def quicksort
+    p self
     return self if self.length <= 1
     pivot = self[0]
     left = self.drop(1).select { |el| el <= pivot }
@@ -13,8 +14,25 @@ class Array
   end
 end
 
-a = (0..5).to_a.shuffle
-p a.quicksort
-
 # this method is fine, however with a sorted input will
 # produce a worst case scenario. a better approach is to select a random pivot
+
+class Array
+  def quicksort_random
+    p self
+    return self if self.length <= 1
+    pivot = rand(self.length)
+    self[0], self[pivot] = self[pivot], self[0]
+    pivot = self[0]
+    left = self.drop(1).select { |el| el <= pivot }
+    right = self.drop(1).select { |el| el > pivot }
+
+    left.quicksort_random + [pivot] + right.quicksort_random
+  end
+end
+
+
+a = (0..10).to_a
+p a.quicksort
+puts ""
+p a.quicksort_random
