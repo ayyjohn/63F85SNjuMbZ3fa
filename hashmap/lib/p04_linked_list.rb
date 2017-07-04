4efa128ae80d1588389aa9f19a5818f17e523e02
Alec Johnson <alecjohnson55@gmail.com>
2017-07-04 08:42:45 -0700

    initial commit

--- hashmap/lib/p04_linked_list.rb
+++ hashmap/lib/p04_linked_list.rb
@@ -20,6 +20,10 @@ end
 
 class LinkedList
   def initialize
+    @head = Node.new
+    @tail = Node.new
+    @head.next = @tail
+    @tail.prev = @head
   end
 
   def [](i)
@@ -28,30 +32,75 @@ class LinkedList
   end
 
   def first
+    @head.next
   end
 
   def last
+    @tail.prev
   end
 
   def empty?
+    @head.next == @tail && @tail.prev == @head
   end
 
   def get(key)
+    current = @head
+    until current == @tail
+      return current.val if current.key == key
+      current = current.next
+    end
+    nil
   end
 
   def include?(key)
+    current = @head
+    until current == @tail
+      current = current.next
+      return true if current.key == key
+    end
+    false
   end
 
   def append(key, val)
+    new = Node.new(key, val)
+    @tail.prev.next = new
+    new.prev = @tail.prev
+    @tail.prev = new
+    new.next = @tail
   end
 
   def update(key, val)
+    current = @head
+    until current.key == key || current == @tail
+      current = current.next
+    end
+    current.val = val
   end
 
   def remove(key)
+    current = @head
+    until current.key == key
+      current = current.next
+    end
+    current.prev.next = current.next
+    current.next.prev = current.prev
+  end
+
+  def inspect
+    current = @head
+    until current == @tail
+      p [current.key, current.val]
+      current = current.next
+    end
+    puts ""
   end
 
   def each
+    current = @head
+    until current.next == @tail
+      current = current.next
+      yield current
+    end
   end
 
   # uncomment when you have `each` working and `Enumerable` included
